43f499652d045e942e059b740f9a00d75f2d27ab
Douhan Wicht <douhan.wicht@gmail.com>
2025-10-28 20:58:47 +0100

    Use gruvbox color scheme

--- scripts/clipboard_menu.sh
+++ scripts/clipboard_menu.sh
@@ -2,7 +2,7 @@
 
 # Clipboard History Picker for MangoWC (Wayland + Rofi)
 
-selection=$(cliphist list | rofi -dmenu -p "Clipboard History" -width 70 -lines 15)
+selection=$(cliphist list | rofi -dmenu -p "Clipboard History" -width 70 -lines 15 -theme ~/.config/mango/rofi/gruvbox-dark-hard.rasi)
 
 if [ -n "$selection" ]; then
   cliphist decode <<< "$selection" | wl-copy
